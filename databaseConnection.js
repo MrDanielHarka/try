10033bb2e5ca6918b0d9ae4d22f117fb9f47b75f
MrDanielHarka <daniel@harka.com>
2023-05-30 13:45:26 +0200

    Edit database connection URL.

--- databaseConnection.js
+++ databaseConnection.js
@@ -1,18 +1,17 @@
 import mysql from 'mysql2/promise';
-import {config} from 'dotenv';
-import {logEventToLogFile} from "./utilities.js";
-import DatabaseException from "./exceptions/DatabaseException.js";
+import { config } from 'dotenv';
+import { logEventToLogFile } from './utilities.js';
+import DatabaseException from './exceptions/DatabaseException.js';
 
 config();
 
 const createConnection = async () => {
   try {
-    return await mysql.createConnection(process.env.DATABASE_URL_LOCAL);
+    return await mysql.createConnection(process.env.DATABASE_URL);
   } catch (error) {
-    logEventToLogFile(error, 'Cannot connect to database.')
+    logEventToLogFile(error, 'Cannot connect to database.');
     throw new DatabaseException();
   }
 };
-
 
 export default createConnection;
